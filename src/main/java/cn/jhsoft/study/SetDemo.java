abc05c3261229fe904baa0b0fc2dab5f2d9554db
hn1987 <hn1987@126.com>
2017-08-12 09:26:27 +0800

    object

--- src/main/java/cn/jhsoft/study/SetDemo.java
+++ src/main/java/cn/jhsoft/study/SetDemo.java
@@ -34,37 +34,37 @@ public class SetDemo {
 
 
 
-//        HashSet hs = new HashSet();
-//        hs.add(new Person("hs1", 57));
-//        hs.add(new Person("hs2", 27));
-//        hs.add(new Person("hs3", 47));
-//        hs.add(new Person("hs4", 37));
-//        hs.add(new Person("hs2", 27));
-//
-//        Iterator it = hs.iterator();
-//        while (it.hasNext()) {
-//            Person p = (Person) it.next();
-//            System.out.println(p.getName()+"--"+p.getAge());
-//        }
-
-
-        HashSet hs = new LinkedHashSet();
-        hs.add("abc1");
-        hs.add("abc2");
-        hs.add("abc3");
-        hs.add("abc1");
-        System.out.println(hs);
+        HashSet hs = new HashSet();
+        hs.add(new Person("hs1", 57));
+        hs.add(new Person("hs2", 27));
+        hs.add(new Person("hs3", 47));
+        hs.add(new Person("hs4", 37));
+        hs.add(new Person("hs2", 27));
 
         Iterator it = hs.iterator();
         while (it.hasNext()) {
-            System.out.println(it.next());
+            Person p = (Person) it.next();
+            System.out.println(p.getName()+"--"+p.getAge());
         }
 
-        String s = "ab";
-        System.out.println(s.hashCode());
-        s = "ba";
-        System.out.println(s.hashCode());
-        System.out.println(s.equals("ha"));
+
+//        HashSet hs = new LinkedHashSet();
+//        hs.add("abc1");
+//        hs.add("abc2");
+//        hs.add("abc3");
+//        hs.add("abc1");
+//        System.out.println(hs);
+//
+//        Iterator it = hs.iterator();
+//        while (it.hasNext()) {
+//            System.out.println(it.next());
+//        }
+//
+//        String s = "ab";
+//        System.out.println(s.hashCode());
+//        s = "ba";
+//        System.out.println(s.hashCode());
+//        System.out.println(s.equals("ha"));
 
     }
 
